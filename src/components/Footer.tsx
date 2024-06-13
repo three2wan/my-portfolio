010e126e33aff5012581fa8c2a00a5d628661a19
Syazwan <syazwan@pertamadigital.com>
2024-06-13 13:22:13 +0800

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,27 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="px-2 py-2">
-      <div className="items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
-        <p>&copy; {currentYear} Syzwan Jamil</p>
-        <p>
-          Developed with
-          <img src={reactLogo} className="w-5 h-5" alt="React logo" /> and
-          <img src={heartLogo} className="w-5 h-5" alt="Heart logo" />
-        </p>
+    <footer>
+      <div className="px-2 py-2">
+        <div className="items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
+          <div className="">&copy; {currentYear} Syzwan Jamil</div>
+          <div>
+            <span>Developed with</span>
+            <img
+              src={reactLogo}
+              className="w-5 h-5 mx-1 inline"
+              alt="React logo"
+            />
+            <span>and</span>
+            <img
+              src={heartLogo}
+              className="w-5 h-5 mx-1 inline"
+              alt="Heart logo"
+            />
+          </div>
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
