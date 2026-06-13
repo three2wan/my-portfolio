2a9bbc5354a980f3b83fdb8ca73c7e5d4ed109ce
Syazwan <syazwan@pertamadigital.com>
2026-06-13 21:34:08 +0800

    New init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-white text-gray-900">
+      <h1 className="text-4xl font-bold p-8">My Portfolio</h1>
+    </main>
+  );
+}
+
+export default App;
