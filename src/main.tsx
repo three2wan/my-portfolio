fd8581d3ac8a90adee0f43d0e7e9d3152c6f3511
Syazwan <syazwan@pertamadigital.com>
2026-06-21 13:47:46 +0800

    Install GA4 dependency and wire up the measurement ID

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import './index.css'
 import App from './App.tsx'
+
+ReactGA.initialize('G-Z0CM9E5CPJ')
+ReactGA.send({ hitType: 'pageview', page: window.location.pathname })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
